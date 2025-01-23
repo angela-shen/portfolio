b60128dfc936e6da5ae12bc9d71f3086422b684b
angela-shen <136782411+angela-shen@users.noreply.github.com>
2025-01-22 23:50:27 -0800

    incorrect path fixed

--- global.js
+++ global.js
@@ -2,12 +2,33 @@ console.log('IT’S ALIVE!');
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
-}
+ }
 
-let navLinks = $$("nav a");
+// let navLinks = $$("nav a");
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
-  );
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
 
-currentLink?.classList.add('current');
+// currentLink?.classList.add('current');
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    // add the rest of your pages here
+  ];
+
+  let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // Create link and add it to nav
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  }
+
+  
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
