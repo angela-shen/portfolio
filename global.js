4f994acea8d8288f811b7f57ada50365a1684b23
angela-shen <136782411+angela-shen@users.noreply.github.com>
2025-01-23 01:59:44 -0800

    not a fix

--- global.js
+++ global.js
@@ -28,10 +28,7 @@ for (let p of pages) {
     let url = p.url;
     let title = p.title;
     // Create link and add it to nav
-    let a = document.createElement('a');
-    a.href = url;
-    a.textContent = title;
-    nav.append(a);
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
   }
 
 const ARE_WE_HOME = document.documentElement.classList.contains('home');
