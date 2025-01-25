3c5c91eac93b3699ac58b7192f52e24c5ca9b29b
angela-shen <136782411+angela-shen@users.noreply.github.com>
2025-01-24 18:12:42 -0800

    add select

--- global.js
+++ global.js
@@ -25,34 +25,38 @@ let nav = document.createElement('nav');
 document.body.prepend(nav);
 
 for (let p of pages) {
-    let url = p.url;
-    let title = p.title;
-    const ARE_WE_HOME = document.documentElement.classList.contains('home');
+  let url = p.url;
+  let title = p.title;
+  const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
-    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
-    // Create link and add it to nav
-    let a = document.createElement('a');
-    a.href = url;
-    a.textContent = title;
-    if (a.host === location.host && a.pathname === location.pathname) {
-      a.classList.add('current');
-    }
-    if (a.host != location.host) {
-      a.target = "_blank";
-    }
-    nav.append(a);
+  url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+  // Create link and add it to nav
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
   }
+  if (a.host != location.host) {
+    a.target = "_blank";
+  }
+  nav.append(a);
+}
 
-  document.body.insertAdjacentHTML(
-    'afterbegin',
-    `
-    <label class="color-scheme">
-      Theme:
-      <select>
-        <option value = 'light dark'>Automatic</option>
-        <option value = 'light'>Light</option>
-        <option value = 'dark'>Dark</option>
-      </select>
-    </label>`
-  );
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value = 'light dark'>Automatic</option>
+      <option value = 'light'>Light</option>
+      <option value = 'dark'>Dark</option>
+    </select>
+  </label>`
+);
   
+select.addEventListener('input', function (event) {
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+});
+
