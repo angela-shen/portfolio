0af78c256fb5e2ead85b3116e91d70095b320096
angela-shen <136782411+angela-shen@users.noreply.github.com>
2025-01-31 22:11:54 -0800

    step 3

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
-renderProjects(projects, projectsContainer, 'h2');
+renderProjects(latestProjects, projectsContainer, 'h2');
+
+const githubData = await fetchGitHubData('angela-shen');
+const profileStats = document.querySelector('#profile-stats');
+if (profileStats) {
+    profileStats.innerHTML = `
+          <dl>
+            <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+            <dt>Followers:</dt><dd>${githubData.followers}</dd>
+            <dt>Following:</dt><dd>${githubData.following}</dd>
+            <dt>Bio:</dt><dd>${githubData.bio}</dd>
+          </dl>
+      `;
+  }
